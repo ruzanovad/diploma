e311e30de4d37a91404a082a82802ee25357f3de
Daria Ruzanova <kaiser7lu@gmail.com>
2024-11-25 13:09:24 +0600

    added first_versions_of_files

---
+++ config.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+# This module offers classes representing filesystem paths
+# with semantics appropriate for different operating systems.
+# Path classes are divided between pure paths, which provide
+# purely computational operations without I/O, and concrete paths,
+# which inherit from pure paths but also provide I/O operations.
+
+
+def get_config():
+    return {
+        "batch_size": 8,
+        "num_epochs": 20,
+        "lr": 10**-4,
+        "seq": 350,
+        "d_model": 512,
+        "datasource": "opus-100",
+        "lang_src": "en",
+        "lang_tgt": "ru",
+        "model_folder": "weights",
+        "model_basename": "tmodel_",
+        "preload": "latest",
+        "tokenizer_file": "tokenizer_{0}.json",
+        "experiment_name": "runs/tmodel",
+    }
+
+
+def get_weights_file_path(config, epoch: str):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}{epoch}.pt"
+    return str(Path(".") / model_folder / model_filename)
+
+
+# Find the latest weights file in the weights folder
+def latest_weights_file_path(config):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}*"
+    weights_files = list(Path(model_folder).glob(model_filename))
+    if len(weights_files) == 0:
+        return None
+    weights_files.sort()
+    return str(weights_files[-1])
