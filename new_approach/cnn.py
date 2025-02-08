b975ded948fa85a1b3ee00dfb6fde7247202a738
Daria Ruzanova <kaiser7lu@gmail.com>
2025-02-08 12:47:48 +0600

    updated code for dataset creation

---
+++ new_approach/cnn.py
@@ -0,0 +1,45 @@
+import cv2
+import os
+import numpy as np
+
+patterns_dir = "number_patterns"
+
+types = {0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9"}
+
+
+def get_bounding_boxes(filename, threshold=0.8):
+    boxes = []
+    for pattern in types.keys():
+        # Загрузим изображение и шаблон
+        image = cv2.imread(filename, 0)
+        template = cv2.imread(os.path.join(patterns_dir, types[pattern], "0.png"), 0)
+        
+        w, h = template.shape[::-1]
+        img_w, img_h = image.shape[::-1]
+        
+        if img_w < w or img_h < h:
+            continue
+
+        # Выполним сопоставление
+        res = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
+
+        # Установим порог обнаружения
+        threshold = 0.8
+        loc = np.where(res >= threshold)
+
+        for pt in zip(*loc[::-1]):
+            boxes.append(
+                list(
+                    map(
+                        str,
+                        [
+                            types[pattern],
+                            str(pt[0] + w * 0.5),
+                            str(pt[1] + h * 0.5),
+                            str(w),
+                            str(h),
+                        ],
+                    )
+                )
+            )
+    return boxes
--- new_approach/latex_generation.py
+++ new_approach/latex_generation.py
@@ -4,6 +4,9 @@ import glob
 import json
 import random
 import re
+from PIL import Image
+
+import cnn
 
 
 latex = r"""\documentclass{standalone}
@@ -14,46 +17,47 @@ latex = r"""\documentclass{standalone}
 """
 
 
-# def parse_dvi_log(dvi_log):
-#     symbols = []
-#     dvi_to_pixel = 0.00006334  # Conversion factor
+def parse_dvi_log(dvi_log):
+    symbols = []
+    dvi_to_pixel = 0.00006334  # Conversion factor
 
-#     for line in dvi_log.split("\n"):
-#         match = re.search(r"setchar(\d+) h:=(-?\d+)", line)
-#         if match:
-#             # print("!")
-#             char_code = int(match.group(1))  # ASCII code of the symbol
-#             x_position = int(match.group(2))  # DVI horizontal position
+    for line in dvi_log.split("\n"):
+        match = re.search(r"setchar(\d+) h:=(-?\d+)", line)
+        if match:
+            # print("!")
+            char_code = int(match.group(1))  # ASCII code of the symbol
+            x_position = int(match.group(2))  # DVI horizontal position
 
-#             # Convert DVI coordinates to pixels
-#             x_pixel = x_position * dvi_to_pixel
-#             symbol = chr(char_code)  # Convert ASCII code to character
+            # Convert DVI coordinates to pixels
+            x_pixel = x_position * dvi_to_pixel
+            symbol = chr(char_code)  # Convert ASCII code to character
 
-#             # Approximate bounding box size (based on font size)
-#             bbox_width = 10  # Estimated width
-#             bbox_height = 14  # Estimated height
+            # Approximate bounding box size (based on font size)
+            bbox_width = 10  # Estimated width
+            bbox_height = 14  # Estimated height
 
-#             # Store extracted data
-#             symbols.append(
-#                 {
-#                     "symbol": symbol,
-#                     "x_min": int(x_pixel),
-#                     "y_min": 0,  # Adjust with real vertical parsing
-#                     "x_max": int(x_pixel + bbox_width),
-#                     "y_max": 0 + bbox_height,  # Adjust as needed
-#                 }
-#             )
+            # Store extracted data
+            symbols.append(
+                {
+                    "symbol": symbol,
+                    "x_min": int(x_pixel),
+                    "y_min": 0,  # Adjust with real vertical parsing
+                    "x_max": int(x_pixel + bbox_width),
+                    "y_max": 0 + bbox_height,  # Adjust as needed
+                }
+            )
 
-#     return symbols
+    return symbols
 
 
 def delete_files_with_extension(directory, extension):
     for file_path in glob.glob(os.path.join(directory, f"*.{extension}")):
         try:
             os.remove(file_path)
-            print(f"Deleted: {file_path}")
+            # print(f"Deleted: {file_path}")
         except Exception as e:
-            print(f"Error deleting {file_path}: {e}")
+            pass
+            # print(f"Error deleting {file_path}: {e}")
 
 
 def generate_pattern(level="number"):
@@ -70,7 +74,7 @@ def generate_pattern(level="number"):
                     current_file = os.path.join(current_dir, str(code))
 
                     tex_file = f"{current_file}.tex"
-                    log_file = f"{current_file}-dvi.log"
+                    txt_file = f"{current_file}.txt"
                     with open(tex_file, "w") as f:
                         f.write(latex % ("$" + number + "$"))
 
@@ -115,35 +119,42 @@ def generate_pattern(level="number"):
                         )
                         continue
 
-                    # dvitype my_symbols.dvi > symbols_positions.txt
-                    log_file_path = os.path.join(current_dir, os.path.basename(log_file))
-                    with open(log_file_path, "w") as log_f:
-                        log_result = subprocess.run(
-                            ["dvitype", os.path.basename(dvi_file)],
-                            stdout=log_f,  # Redirect output to the log file
-                            stderr=subprocess.PIPE,
-                            cwd=current_dir,
-                        )
-                    with open(log_file_path, "r") as log_f:
-                        dvi_log_content = log_f.read()
+                    # # dvitype my_symbols.dvi > symbols_positions.txt
+                    # log_file_path = os.path.join(current_dir, os.path.basename(log_file))
+                    # with open(log_file_path, "w") as log_f:
+                    #     log_result = subprocess.run(
+                    #         ["dvitype", os.path.basename(dvi_file)],
+                    #         stdout=log_f,  # Redirect output to the log file
+                    #         stderr=subprocess.PIPE,
+                    #         cwd=current_dir,
+                    #     )
+                    # with open(log_file_path, "r") as log_f:
+                    #     dvi_log_content = log_f.read()
 
                     # Parse the extracted log content
                     # print(parse_dvi_log(dvi_log_content))
-                    if tex_result.returncode != 0:
-                        print(
-                            f"Error creating log {log_file}: {log_result.stderr.decode()}"
-                        )
-                        continue
+                    
+                    # if tex_result.returncode != 0:
+                    #     print(
+                    #         f"Error creating log {log_file}: {log_result.stderr.decode()}"
+                    #     )
+                    #     continue
+
+                    im = Image.open(png_file)
+                    width, height = im.size
+
+                    with open(txt_file, "w") as file:
+                        file.write(f"{number} {width/2} {height/2} {width} {height}\n")
 
                     # Clean up
-                    # for ext in ["aux", "log", "dvi", "tex"]:
-                    #     delete_files_with_extension(current_dir, ext)
+                    for ext in ["aux", "log", "dvi", "tex"]:
+                        delete_files_with_extension(current_dir, ext)
 
         case _:
             raise Exception("Unsupported pattern level")
 
 
-def generate_dataset(level="number", count=42, seed=42):
+def generate_dataset(level="number", count=1000, seed=42):
     random.seed(seed)
     match level:
         case "number":
@@ -199,6 +210,12 @@ def generate_dataset(level="number", count=42, seed=42):
                     )
                     continue
 
+                txt_file = f"{current_file}.txt"  
+                bounding_boxes = cnn.get_bounding_boxes(png_file)
+                with open(txt_file, "w") as file:
+                    for box in bounding_boxes:
+                        file.write(' '.join(box) +'\n')
+                
                 # Clean up
                 for ext in ["aux", "log", "dvi", "tex"]:
                     delete_files_with_extension(base_dir, ext)
@@ -208,5 +225,5 @@ def generate_dataset(level="number", count=42, seed=42):
 
 
 # Run the function
-generate_pattern()
-# generate_dataset()
+# generate_pattern()
+generate_dataset()
