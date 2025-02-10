fb88109654c18e6bbe2d8416e39f952ddcfaf86a
Daria Ruzanova <kaiser7lu@gmail.com>
2025-02-10 14:51:33 +0600

    updated bounding boxes to yolo format

--- new_approach/bounding_box.py
+++ new_approach/bounding_box.py
@@ -6,16 +6,20 @@ patterns_dir = "number_patterns"
 
 types = {0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9"}
 
+
 def get_bounding_boxes(filename, threshold=0.9):
+    """
+    Get bounding box in YOLO format
+    """
     boxes = []
     for pattern in types.keys():
         # Загрузим изображение и шаблон
         image = cv2.imread(filename, 0)
         template = cv2.imread(os.path.join(patterns_dir, types[pattern], "0.png"), 0)
-        
+
         w, h = template.shape[::-1]
         img_w, img_h = image.shape[::-1]
-        
+
         if img_w < w or img_h < h:
             continue
 
@@ -33,10 +37,10 @@ def get_bounding_boxes(filename, threshold=0.9):
                         str,
                         [
                             types[pattern],
-                            str(pt[0] + w * 0.5),
-                            str(pt[1] + h * 0.5),
-                            str(w),
-                            str(h),
+                            str((pt[0] + w * 0.5) / img_w),
+                            str((pt[1] + h * 0.5) / img_h),
+                            str(w / img_w),
+                            str(h / img_h),
                         ],
                     )
                 )
