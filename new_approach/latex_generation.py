f6902d5ec71b3e031d634d596434c6dd2e1deb04
Daria Ruzanova <kaiser7lu@gmail.com>
2025-02-24 12:23:08 +0600

    feat!: added environment variables

--- new_approach/latex_generation.py
+++ new_approach/latex_generation.py
@@ -3,8 +3,11 @@ import os
 import glob
 import random
 from PIL import Image
-import bounding_box
+from dotenv import load_dotenv
+import utils
 
+
+load_dotenv()
 
 latex = r"""\documentclass{standalone}
 \usepackage{amsmath}
@@ -12,8 +15,6 @@ latex = r"""\documentclass{standalone}
 %s
 \end{document}
 """
-
-DATASETS = "datasets"
 
 
 def delete_files_with_extension(directory, extension):
@@ -26,77 +27,74 @@ def delete_files_with_extension(directory, extension):
             # print(f"Error deleting {file_path}: {e}")
 
 
-def generate_pattern(level="number"):
-    match level:
-        case "number":
-            base_dir = os.path.join(DATASETS, "number_patterns")
-            os.makedirs(base_dir, exist_ok=True)
+def generate_pattern():
+    base_dir = os.getenv("patterns_folder")
 
-            for number in map(str, range(0, 10)):
-                current_dir = os.path.join(base_dir, number)
-                os.makedirs(current_dir, exist_ok=True)
+    os.makedirs(base_dir, exist_ok=True)
 
-                for code in range(1):
-                    current_file = os.path.join(current_dir, str(code))
+    templates = utils.load_symbols_from_templates(os.getenv("templates"))
 
-                    tex_file = f"{current_file}.tex"
-                    txt_file = f"{current_file}.txt"
-                    with open(tex_file, "w") as f:
-                        f.write(latex % ("$" + number + "$"))
+    for token, class_number in templates.items():
+        # for number in map(str, range(0, 10)):
+        current_dir = os.path.join(base_dir, str(class_number))
+        os.makedirs(current_dir, exist_ok=True)
 
-                    # Compile .tex to .dvi
-                    tex_result = subprocess.run(
-                        [
-                            "latex",
-                            "-interaction=nonstopmode",
-                            os.path.basename(tex_file),
-                        ],
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE,
-                        cwd=current_dir,
-                    )
-                    if tex_result.returncode != 0:
-                        print(
-                            f"Error compiling {tex_file}: {tex_result.stderr.decode()}"
-                        )
-                        continue
+        for code in range(1):
+            current_file = os.path.join(current_dir, str(code))
 
-                    # Convert .dvi to .png
-                    dvi_file = f"{current_file}.dvi"
-                    png_file = f"{current_file}.png"
-                    dvi_result = subprocess.run(
-                        [
-                            "dvipng",
-                            "-T",
-                            "tight",
-                            "-D",
-                            "300",
-                            "-o",
-                            os.path.basename(png_file),
-                            os.path.basename(dvi_file),
-                        ],
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE,
-                        cwd=current_dir,
-                    )
-                    if dvi_result.returncode != 0:
-                        print(
-                            f"Error converting {dvi_file} to PNG: {dvi_result.stderr.decode()}"
-                        )
-                        continue
+            tex_file = f"{current_file}.tex"
+            txt_file = f"{current_file}.txt"
+            with open(tex_file, "w") as f:
+                f.write(latex % ("$" + token + "$"))
 
-                    im = Image.open(png_file)
-                    width, height = im.size
+            # Compile .tex to .dvi
+            tex_result = subprocess.run(
+                [
+                    "latex",
+                    "-interaction=nonstopmode",
+                    os.path.basename(tex_file),
+                ],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                cwd=current_dir,
+            )
+            if tex_result.returncode != 0:
+                print(f"Error compiling {tex_file}: {tex_result.stderr.decode()}")
+                continue
 
-                    with open(txt_file, "w") as file:
-                        file.write(f"{number} {width/2} {height/2} {width} {height}\n")
+            # Convert .dvi to .png
+            dvi_file = f"{current_file}.dvi"
+            png_file = f"{current_file}.png"
+            dvi_result = subprocess.run(
+                [
+                    "dvipng",
+                    "-T",
+                    "tight",
+                    "-D",
+                    "300",
+                    "-o",
+                    os.path.basename(png_file),
+                    os.path.basename(dvi_file),
+                ],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                cwd=current_dir,
+            )
+            if dvi_result.returncode != 0:
+                print(
+                    f"Error converting {dvi_file} to PNG: {dvi_result.stderr.decode()}"
+                )
+                continue
 
-                    # Clean up
-                    for ext in ["aux", "log", "dvi", "tex"]:
-                        delete_files_with_extension(current_dir, ext)
+            im = Image.open(png_file)
+            width, height = im.size
 
-        case _:
-            raise Exception("Unsupported pattern level")
+            with open(txt_file, "w") as file:
+                file.write(f"{class_number} {width/2} {height/2} {width} {height}\n")
+
+            # Clean up
+            for ext in ["aux", "log", "dvi", "tex"]:
+                delete_files_with_extension(current_dir, ext)
 
 
 def generate_one(current_prefix: str, template):
@@ -104,7 +102,7 @@ def generate_one(current_prefix: str, template):
     current_file = current_prefix
     tex_file = f"{current_file}.tex"
     with open(tex_file, "w") as f:
-        f.write(latex % template)
+        f.write(latex % ("$" + template + "$"))
 
     # Compile .tex to .dvi
     tex_result = subprocess.run(
@@ -147,12 +145,24 @@ def generate_one(current_prefix: str, template):
         delete_files_with_extension("", ext)
 
 
-def fill_file(images_dir, labels_dir, code, level, unique_numbers):
-    current_file = os.path.join(images_dir, "%d_%s" % (code, level))
+def fill_file(images_dir, labels_dir, code):
 
+    choice = random.randint(0, 2)
+    if choice == 0:
+        content = generate_number()
+        suffix = "number"
+    elif choice == 1:
+        content = generate_decimal()
+        suffix = "decimal"
+    else:
+        content = generate_word()
+        suffix = "word"
+
+    temp_name = "%d_%s" % (code, suffix)
+    current_file = os.path.join(images_dir, temp_name)
     tex_file = f"{current_file}.tex"
     with open(tex_file, "w") as f:
-        f.write(latex % unique_numbers[code])
+        f.write(latex % ("$" + content + "$"))
 
     # Compile .tex to .dvi
     tex_result = subprocess.run(
@@ -191,62 +201,67 @@ def fill_file(images_dir, labels_dir, code, level, unique_numbers):
         print(f"Error converting {dvi_file} to PNG: {dvi_result.stderr.decode()}")
         return
 
-    txt_file = os.path.join(labels_dir, "%d_%s" % (code, level)) + ".txt"
-    bounding_boxes = bounding_box.get_bounding_boxes(png_file)
+    txt_file = os.path.join(labels_dir, temp_name) + ".txt"
+    bounding_boxes = utils.get_bounding_boxes(png_file)
 
     with open(txt_file, "w") as file:
         for box in bounding_boxes:
             file.write(" ".join(box) + "\n")
 
 
-def generate_dataset(level="number", count=100, seed=42, train=80, val=20):
+def generate_number() -> str:
+    return str(random.randint(0, 9999))
+
+
+def generate_decimal() -> str:
+    integer_part = random.randint(0, 9999)
+    decimal_part = random.randint(0, 9999)
+    return f"{integer_part}.{decimal_part}"
+
+
+def generate_word(length=5) -> str:
+    length = random.randint(1, 5)
+    return "".join(
+        random.choices("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", k=length)
+    )
+
+
+def generate_dataset(level="number", count=1000, seed=42, train=80, val=20):
     assert train + val == 100
     random.seed(seed)
-    match level:
-        case "number":
-            base_dir = os.path.join(DATASETS, "dataset")
-            images_dir = os.path.join(base_dir, "images")
-            labels_dir = os.path.join(base_dir, "labels")
+    base_dir = os.getenv("yolo_dataset_folder")
+    images_dir = os.path.join(base_dir, "images")
+    labels_dir = os.path.join(base_dir, "labels")
 
-            images_train_dir = os.path.join(images_dir, "train")
-            images_val_dir = os.path.join(images_dir, "val")
-            labels_train_dir = os.path.join(labels_dir, "train")
-            labels_val_dir = os.path.join(labels_dir, "val")
+    images_train_dir = os.path.join(images_dir, "train")
+    images_val_dir = os.path.join(images_dir, "val")
+    labels_train_dir = os.path.join(labels_dir, "train")
+    labels_val_dir = os.path.join(labels_dir, "val")
 
-            os.makedirs(images_train_dir, exist_ok=True)
-            os.makedirs(images_val_dir, exist_ok=True)
-            os.makedirs(labels_train_dir, exist_ok=True)
-            os.makedirs(labels_val_dir, exist_ok=True)
+    os.makedirs(images_train_dir, exist_ok=True)
+    os.makedirs(images_val_dir, exist_ok=True)
+    os.makedirs(labels_train_dir, exist_ok=True)
+    os.makedirs(labels_val_dir, exist_ok=True)
 
-            unique_numbers = random.sample(range(1, 10001), count)
+    train_number = (count * train) // 100
+    val_number = count - train_number
+    for code in range(train_number):
+        fill_file(images_train_dir, labels_train_dir, code)
 
-            train_number = (count * train) // 100
-            val_number = count - train_number
-            for code in range(train_number):
-                fill_file(
-                    images_train_dir, labels_train_dir, code, level, unique_numbers
-                )
+    for code in range(train_number, train_number + val_number):
+        fill_file(images_val_dir, labels_val_dir, code)
 
-            for code in range(train_number, train_number + val_number):
-                fill_file(images_val_dir, labels_val_dir, code, level, unique_numbers)
+    # Clean up
+    for ext in ["aux", "log", "dvi", "tex"]:
+        delete_files_with_extension(images_train_dir, ext)
+        delete_files_with_extension(images_val_dir, ext)
 
-            # Clean up
-            for ext in ["aux", "log", "dvi", "tex"]:
-                delete_files_with_extension(images_train_dir, ext)
-                delete_files_with_extension(images_val_dir, ext)
-
-            with open("dataset.yaml", "w") as file:
-                file.write("path: dataset\n")
-                file.write("train: images/train\n")
-                file.write("val: images/val\n\n")
-                file.write("names:\n")
-                for key in sorted(bounding_box.types.keys()):
-                    file.write(f"  {key}: {bounding_box.types[key]}\n")
-
-        case _:
-            raise Exception("Unsupported pattern level")
+    utils.generate_yolo_yaml(
+        os.getenv("templates"), os.getenv("dataset_folder"), "dataset.yaml"
+    )
 
 
 if __name__ == "__main__":
-    generate_pattern()
+    # generate_pattern()
     generate_dataset()
+    # print(load_symbols_from_templates(os.getenv("templates")))
